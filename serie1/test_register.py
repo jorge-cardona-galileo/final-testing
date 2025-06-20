52ee5a6a335a7970cf775ca218464c735228ad9d
Jorge <jorgemario.cardonaoliva@gmail.com>
2025-06-19 21:16:15 -0600

    Serie 1

--- serie1/test_register.py
+++ serie1/test_register.py
@@ -2,19 +2,49 @@ from playwright.sync_api import Page, expect, sync_playwright
 import time
 
 URL = "https://ecommerce-playground.lambdatest.io/"
+import random
+import string
+
+def generate_random_email():
+    local_part_length = random.randint(5, 15)  # Random length between 5 and 15 characters
+    local_part = ''.join(random.choices(string.ascii_lowercase + string.digits, k=local_part_length))
+    domain_length = random.randint(4, 10)  # Random length between 4 and 10 characters
+    domain = ''.join(random.choices(string.ascii_lowercase, k=domain_length))
+    tlds = ['com', 'net', 'org']
+    tld = random.choice(tlds)
+    email = f"{local_part}@{domain}.{tld}"
+    return email
+
 class Register:
-    def __init___(self, page:Page):
+    def __init__(self, page: Page):
         self.page = page
+    
+    def wait_for_page_load(self, seconds=2):
+        self.page.wait_for_load_state("networkidle")
+        time.sleep(seconds) #Tiempo de espera para asegurar que la página se haya cargado completamente
 
-    def test_create_account():
-        page.goto(URL)
-        page.locator("span:has-text('My account')").click()
-        time(5)
-
+    def test_create_account(self):
+        self.page.goto(URL)
+        self.wait_for_page_load()
+        self.page.locator("span:has-text('My account')").nth(1).click()
+        self.wait_for_page_load()
+        self.page.locator("a:has-text('Continue')").click()
+        self.wait_for_page_load()
+        page.fill('input[name="firstname"]', "Jorge")
+        page.fill('input[name="lastname"]', "Cardona")
+        page.fill('input[name="email"]', generate_random_email())
+        page.fill('input[name="telephone"]', "+50254588888")
+        page.fill('input[name="password"]', "Fake_password1")
+        page.fill('input[name="confirm"]', "Fake_password1")
+        page.check('input[type="checkbox"]#input-agree')
+        page.click('button[type="submit"]')
+        self.wait_for_page_load()
+        expect(page.locator("span:has-text('Your Account Has Been Created!')")).to_be_visible()
+    
 if __name__ == '__main__':
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        regiter = Register(page)
-        regiter.test_create_account()
+        register = Register(page)
+        register.test_create_account()
         browser.close()
